2fe5d6e1d612c9839b64fd7e2981820029356d16
skssandy <89083444+skssandy@users.noreply.github.com>
2022-01-24 18:19:44 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-aws s3 cp s3://tomcattestingbucket/project2/java-hello-world.war /tmp/
+aws s3 cp s3://tomcattestingbucket/Jan24/java-hello-world.war /tmp/
 sudo mv /tmp/java-hello-world.war /home/ubuntu/apache-tomcat-10.0.16/webapps/java-hello-world.war
